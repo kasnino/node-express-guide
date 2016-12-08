743ab210c382ebe31ad69badf04235f6049ff24c
kasnino <kasn.emprendedor@gmail.com>
2016-12-07 23:11:06 -0400

    error h

--- static.js
+++ static.js
@@ -3,5 +3,6 @@ var express = require('express')
 var app = express()
 
 app.use(express.static(process.argv[3] || path.join(__dirname, 'public')))
-
-app.listen(3000)
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
